43be49db12b4e512d88f3a00123988a2d16a37e5
Ted Wei <jdwei@hotmail.com>
2017-02-08 06:34:23 +0800

    Modify src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { TapRevealComponent } from '../components/tap-reveal/tap-reveal';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    TapRevealComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp)
