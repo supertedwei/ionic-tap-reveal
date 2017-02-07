97d2534306b2fe6a246e2fad69c980e9040ce7fc
Ted Wei <jdwei@hotmail.com>
2017-02-08 06:39:13 +0800

    Modify src/components/tap-reveal/tap-reveal.ts

--- src/components/tap-reveal/tap-reveal.ts
+++ src/components/tap-reveal/tap-reveal.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
-
-/*
-  Generated class for the TapReveal component.
-
-  See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
-  for more info on Angular 2 Components.
-*/
+ 
 @Component({
   selector: 'tap-reveal',
   templateUrl: 'tap-reveal.html'
 })
 export class TapRevealComponent {
-
-  text: string;
-
+ 
+  toggled: boolean = false;
+ 
   constructor() {
-    console.log('Hello TapReveal Component');
-    this.text = 'Hello World';
+ 
   }
-
+ 
+  toggle() {
+    this.toggled = !this.toggled;
+  }
+ 
 }
